5b396dbbc5ecc82fb5ce82938993824ce50bbdbc
Victor Mattsson <victor.m.dravnieks@gmail.com>
2015-12-01 10:37:24 +0100

    Initial commit

---
+++ desktop/src/se/victormattsson/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.victormattsson.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.victormattsson.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
